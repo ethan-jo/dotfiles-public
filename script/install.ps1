758b4fb37d85ebe02afe3edd9663e141790c15c9
Ethan <ethan@eee.com>
2022-05-31 20:59:30 +0800

    chore(powershell): Ajust powershell install script

--- script/install.ps1
+++ script/install.ps1
@@ -9,17 +9,20 @@ winget install  -e --id Git.Git
 Install-Module -Name posh-git -Scope CurrentUser
 
 # oh-my-posh
-Install-Module -Name oh-my-posh -Scope CurrentUser
+winget install oh-my-posh
+
+# Terminal Icons
+Install-Module -Name Terminal-Icons -Repository PSGallery
 
 # PSFzf
 Install-Module -Name PSFzf  -Scope CurrentUser
 
 # z
-Install-Module -Name z -Scope CurrentUserrm 
+Install-Module -Name z -Scope CurrentUser
 
 mkdir .config/powershell  -Force
 cp dotfiles-public/.config/powershell/user_profile.ps1 .config/powershell/
-cp dotfiles-public/.config/powershell/haffxu.omp.json .config/
+cp dotfiles-public/.config/powershell/haffxu.omp.json .
 cp dotfiles-public/script/fzf.exe  ./AppData/Local/Programs/oh-my-posh/bin
 
 # options Scoop
