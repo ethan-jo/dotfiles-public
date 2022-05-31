52289c7a2ef0219f5c5d2f31c2ac2ce005e27c63
haffxu <haifeng.xu@iraytek.com>
2022-05-31 10:34:22 +0800

    chore: localization for myself

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -3,7 +3,7 @@
 
 Import-Module posh-git
 Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+$omp_config = Join-Path $env:USERPROFILE ".\haffxu.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
---
+++ script/install.ps1
@@ -0,0 +1,27 @@
+cd ~/
+git clone https://github.com/ethan-xu1986/dotfiles-public.git
+
+# neovim
+winget install Neovim.Neovim
+
+# git
+winget install  -e --id Git.Git
+Install-Module -Name posh-git -Scope CurrentUser
+
+# oh-my-posh
+Install-Module -Name oh-my-posh -Scope CurrentUser
+
+# PSFzf
+Install-Module -Name PSFzf  -Scope CurrentUser
+
+# z
+Install-Module -Name z -Scope CurrentUserrm 
+
+mkdir .config/powershell  -Force
+cp dotfiles-public/.config/powershell/user_profile.ps1 .config/powershell/
+cp dotfiles-public/.config/powershell/haffxu.omp.json .config/
+cp dotfiles-public/script/fzf.exe  ./AppData/Local/Programs/oh-my-posh/bin
+
+# options Scoop
+# iwr -useb get.scoop.sh | iex
+
