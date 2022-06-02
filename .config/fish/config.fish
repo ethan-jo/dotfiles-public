770c499ec5eaa1bbd2b45552237402acfce1aa6d
haffxu <haifeng.xu@iraytek.com>
2022-06-02 16:22:47 +0800

    chore(fish): remove something not for myself

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,30 +15,30 @@ alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
 alias g git
-command -qv nvim && alias vim nvim
+#command -qv nvim && alias vim nvim
 
-set -gx EDITOR nvim
+#set -gx EDITOR nvim
 
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
+#set -gx PATH bin $PATH
+#set -gx PATH ~/bin $PATH
+#set -gx PATH ~/.local/bin $PATH
 
 # NodeJS
-set -gx PATH node_modules/.bin $PATH
+# set -gx PATH node_modules/.bin $PATH
 
 # Go
-set -g GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
+#set -g GOPATH $HOME/go
+#set -gx PATH $GOPATH/bin $PATH
 
 # NVM
-function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-  status --is-command-substitution; and return
-
-  if test -f .nvmrc; and test -r .nvmrc;
-    nvm use
-  else
-  end
-end
+#function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+#  status --is-command-substitution; and return
+#
+#  if test -f .nvmrc; and test -r .nvmrc;
+#    nvm use
+#  else
+#  end
+#end
 
 switch (uname)
   case Darwin
