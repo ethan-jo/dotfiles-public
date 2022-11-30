23e5ab28b9910b6d87c6ff02c73c729c743b97d8
EthanJo <haifeng.xu@iraytek.com>
2022-11-30 17:37:51 +0800

    chore: powershell支持scp-copy-id

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,21 +2,25 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
-Import-Module oh-my-posh
 $omp_config = Join-Path $env:USERPROFILE ".\haffxu.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
+Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -Colors @{
+  InlinePrediction   = '#247C73'
+}
 
 # Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+#Import-Module PSFzf
+#Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
@@ -35,6 +39,10 @@ function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
+function ssh-copy-id($remote){
+  type $env:USERPROFILE\.ssh\id_rsa.pub | ssh $remote "cat >> .ssh/authorized_keys"
+}
+
 function showproxy{netsh winhttp show proxy}
 function resetproxy{netsh winhttp reset proxy}
 function setproxy{netsh winhttp set proxy 127.0.0.1:7890}
